4fc4080782a9e3776b4dc9de5eb7021fd83fd802
James <jamesdstacey2@gmail.com>
2023-10-19 23:44:22 +1100

    completed nav responsiveness

--- nav-challenge-2/main.js
+++ nav-challenge-2/main.js
@@ -1,7 +1,13 @@
-const navToggle = document.querySelector('.nav-toggle');
-const nav = document.querySelector('.nav');
+const navToggle = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".nav");
 
+navToggle.addEventListener("click", () => {
+  nav.classList.toggle("nav--visible");
+});
 
-navToggle.addEventListener('click', () => {
-    nav.classList.toggle('nav--visible');
-})
+// I just had to add this, otherwise the styles applied when the hamburger menu was pressed remain
+// after the 800px breakpoint is reached, even though the hamburger menu
+// was then invisible and unclickable.
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 800) nav.classList.remove("nav--visible");
+});
